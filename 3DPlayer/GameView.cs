c7d607f844a854e09c8a3be737be6daf722347fc
hihijack <linhj2008@yeah.net>
2014-02-23 16:15:08 +0800

    20140223

--- 3DPlayer/GameView.cs
+++ 3DPlayer/GameView.cs
@@ -5,9 +5,7 @@ using System.Collections.Generic;
 using System.Threading;
 
 public enum EGameState{
-	Running,
-	UIInfoing,
-	UIPaging
+	Running
 }
 
 public class GameView : MonoBehaviour
@@ -15,8 +13,24 @@ public class GameView : MonoBehaviour
 	
 	public int VCInput_Axis;
 	public int VCInput_Ver_Axis;
-	public int VCInput_BtnA;
-	public int VCInput_BtnB;
+	private int _vcInputBtnA;
+	public int VCInputBtnA{
+		get{
+			return _vcInputBtnA;
+		}
+		set{
+			_vcInputBtnA = value;
+		}
+	}
+	private int _vcInputBtnB;
+	public int VCInputBtnB{
+		get{
+			return _vcInputBtnB;
+		}
+		set{
+			_vcInputBtnA = value;
+		}
+	}
 	
 	public EGameState gameState;
 	
@@ -57,15 +71,15 @@ public class GameView : MonoBehaviour
 		}
 		
 		if(Input.GetKeyDown(KeyCode.Space)){
-			VCInput_BtnA = 1;
-		}else if(Input.GetKeyUp(KeyCode.Space)){
-			VCInput_BtnA = 0;
+			VCInputBtnA = 1;
+		}else{
+			VCInputBtnA = 0;
 		}
 		
 		if(Input.GetKeyDown(KeyCode.X)){
-			VCInput_BtnB = 1;
-		}else if(Input.GetKeyUp(KeyCode.X)){
-			VCInput_BtnB = 0;
+			VCInputBtnB  = 1;
+		}else{
+			VCInputBtnB = 0;
 		}
 		#endif
 		if(hero != null){
@@ -78,7 +92,11 @@ public class GameView : MonoBehaviour
 //		VCInput_BtnB = 0;
 //		VCInput_Ver_Axis = 0;
 	}
+	
+	
+	void OnGUI(){
 
+	}
 	//×××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××
 	
 	public void OnBtnPress(string btnname, bool isDown){
@@ -113,16 +131,16 @@ public class GameView : MonoBehaviour
 		
 		if("btn_A".Equals(btnname)){
 			if(isDown){
-				VCInput_BtnA = 1;
+				VCInputBtnA = 1;
 			}else{
-				VCInput_BtnA = 0;
+				VCInputBtnA = 0;
 			}
 		}
 		if("btn_B".Equals(btnname)){
 			if(isDown){
-				VCInput_BtnB = 1;
+				VCInputBtnB  = 1;
 			}else{
-				VCInput_BtnB = 0;
+				VCInputBtnB = 0;
 			}
 		}
 	}
@@ -131,59 +149,5 @@ public class GameView : MonoBehaviour
 		return this.gameState == gameState;
 	}
 	
-	void OnDrag(DragGesture drag){
-//		Vector2 deltaMove = drag.DeltaMove * 0.1f;
-//		hero.transform.Rotate(0f, deltaMove.x, 0f, Space.World);
-//		
-//		CameraControll camealControll = main_camera.GetComponent<CameraControll>();
-//		camealControll.OnDrag(drag);
-		
-//		main_camera.transform.Rotate(deltaMove.y, 0, 0f, Space.World);
-//		Vector3 cameraAngle = main_camera.transform.eulerAngles;
-//		cameraAngle.z = 0;
-//		cameraAngle.y = hero.transform.eulerAngles.y;
-//		main_camera.transform.eulerAngles = cameraAngle;
-	}
-	
-	IEnumerator InitWorld(){
-		string strCubesData = PlayerPrefs.GetString("cubes");
-		string[] strCubes = strCubesData.Split('|');
-		foreach (string item in strCubes) {
-			
-			if(string.IsNullOrEmpty(item)){
-				continue;
-			}
-			
-			string[] posVals = item.Split(',');
-			float localX = float.Parse(posVals[0]);
-			float localY = float.Parse(posVals[1]);
-			float localZ = float.Parse(posVals[2]);
-			GameObject cube = Tools.LoadResourcesGameObject("Prefabs/Cube");
-			cube.transform.parent = cubeParent.transform;
-			Vector3 locPos = new Vector3(localX, localY, localZ);
-			cube.transform.localPosition = locPos;
-			yield return 1;
-		}
-		
-		// 创建玩家
-		string posPlayer = PlayerPrefs.GetString("playerpos");
-		string[] posPlayerVals = posPlayer.Split(',');
-		float x = float.Parse(posPlayerVals[0]);
-		float y = float.Parse(posPlayerVals[1]);
-		float z = float.Parse(posPlayerVals[2]);
-		Vector3 playerPos = new Vector3(x, y + 0.6f, z);
-		
-		GameObject gobjHero = Tools.LoadResourcesGameObject("Prefabs/player");
-		gobjHero.transform.position = playerPos;
-		
-		CameraControll camealControll = main_camera.GetComponent<CameraControll>();
-		camealControll.target = gobjHero.transform;
-		camealControll.Init();
-		
-		hero = gobjHero.GetComponent<Hero>();
-		
-		initFinish = true;
-		
-		yield return 1;
-	}
+
 }
