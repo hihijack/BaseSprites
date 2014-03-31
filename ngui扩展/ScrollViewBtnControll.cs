c4c8dcb9d299338fd67b781e972f021facf773a5
hihijack <linhj2008@yeah.net>
2014-03-31 10:16:59 +0800

    add scrollviewbtncontroll

---
+++ ngui扩展/ScrollViewBtnControll.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(UIScrollView))]
+public class ScrollViewBtnControll : MonoBehaviour {
+
+    public UIButton btnBack;
+    public UIButton btnForward;
+    UIGrid grid;
+    public int index;
+
+    UIScrollView usv;
+
+    Vector3 oriPos;
+    float cellWidth;
+    float cellHeigth;
+    // Use this for initialization
+	void Start () {
+        btnBack.onClick.Add(new EventDelegate(OnBack));
+        btnForward.onClick.Add(new EventDelegate(OnForward));
+        usv = GetComponent<UIScrollView>();
+        grid = GetComponentInChildren<UIGrid>();
+        oriPos = transform.localPosition;
+        cellWidth = grid.cellWidth;
+        cellHeigth = grid.cellHeight;
+        Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnBack() {
+        index--;
+        if(index < 0){
+            index = 0;
+        }
+        Init();
+    }
+
+    void OnForward() {
+        index++;
+        int maxIndex = grid.transform.childCount - 1;
+        if(index > maxIndex)
+        {
+            index = maxIndex;
+        }
+        Init();
+    }
+
+    void Init() 
+    {
+        if (usv.movement == UIScrollView.Movement.Horizontal)
+	    {
+            float x = oriPos.x - index * cellWidth;
+            SpringPanel.Begin(usv.gameObject, new Vector3(x, oriPos.y, oriPos.z), 8f);
+	    }
+    }
+}
